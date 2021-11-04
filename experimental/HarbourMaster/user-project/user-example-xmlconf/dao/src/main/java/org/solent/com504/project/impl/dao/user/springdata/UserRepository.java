47af606f497ea56bcf914c4c2de0efd08a85496a
Jake Ashton <4ashtj48@solent.ac.uk>
2021-11-04 18:04:29 +0000

    Update UserRepository.java

--- experimental/HarbourMaster/user-project/user-example-xmlconf/dao/src/main/java/org/solent/com504/project/impl/dao/user/springdata/UserRepository.java
+++ experimental/HarbourMaster/user-project/user-example-xmlconf/dao/src/main/java/org/solent/com504/project/impl/dao/user/springdata/UserRepository.java
@@ -15,3 +15,26 @@ public interface UserRepository extends JpaRepository<User, Long> {
     public List<User> findByNames(@Param("firstName") String firstName, @Param("secondName") String secondName);
     
 }
+
+//
+//package org.solent.com504.project.impl.dao.party.springdata;
+//
+//import java.util.List;
+//import org.solent.com504.project.model.party.dao.PilotAvailability;
+//import org.solent.com504.project.model.party.dto.Pilot;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface PilotRepository {
+//    @Query("We need some sort of Join here")
+//    public List<Pilot> findByPilotAvailability(@Param("pilotAvailability") PilotAvailability pilotAvailability);
+//
+//    @Query("We need some sort of Join here")
+//    public List<Pilot> findByName(@Param("name") String name);
+//
+//
+//    @Query("We need some sort of Join here")
+//    public List<Pilot> findAll();
+//}
