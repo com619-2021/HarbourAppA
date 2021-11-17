cd1409ec4e62f143a2df9963476d0be71d5e3f74
Jake Ashton <4ashtj48@solent.ac.uk>
2021-11-17 16:54:04 +0000

    Update Berth.java

--- experimental/HarbourMaster/project/src/main/java/dto/Berth.java
+++ experimental/HarbourMaster/project/src/main/java/dto/Berth.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dto;
 
-/**
- *
- * @author jake_
- */
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+
+
+@Entity
+@Table(name="berths")
 public class Berth {
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     
+    @Enumerated(EnumType.STRING)
+    private Float lat;
+    private Float lon;
+
+    public Berth(Float lat, Float lon){
+        this.lat = lat;
+        this.lon = lon;
+
+    }
+
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+    }
+    public Float getLat() {
+		return lat;
+	}
+
+	public void setLat(Float lat) {
+		this.lat = lat;
+    }
+    public Float getLon() {
+		return lon;
+	}
+
+	public void setLon(Float lon) {
+		this.lon = lon;
+	}
 }
