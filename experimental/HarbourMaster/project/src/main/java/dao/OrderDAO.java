d5a5170f1340ea6c7a11432abd9ccdc9c51664fc
Jake Ashton <4ashtj48@solent.ac.uk>
2021-11-17 17:26:12 +0000

    orderDAO Ivan

--- experimental/HarbourMaster/project/src/main/java/dao/OrderDAO.java
+++ experimental/HarbourMaster/project/src/main/java/dao/OrderDAO.java
@@ -7,18 +7,47 @@ package dao;
 
 import dto.Order;
 
-/**
- *
- * @author jake_
- */
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import repository.OrderRepository;
+
+
+@Repository
 public class OrderDAO {
-
-    public void save(Order order) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public Order findById(int orderId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    @Autowired
+    private OrderRepository orderRepository;
     
+    public Order findById(int id) {
+		return orderRepository.findById(id).isPresent() ? orderRepository.findById(id).get() : null;
+	}
+
+	public Boolean existsById(int id) {
+		return orderRepository.existsById(id);
+	}
+
+	public Order save(Order order) {
+		return orderRepository.save(order);
+	}
+
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+
+	public void deleteById(int id) {
+		orderRepository.deleteById(id);
+	}
+
+	public void delete(Order order) {
+		orderRepository.delete(order);
+	}
+
+	public void deleteAll() {
+		orderRepository.deleteAll();
+	}
+
+	public List<Order> findByID(int id) {
+		return orderRepository.findByID(id);
+	}
 }
--- experimental/HarbourMaster/project/src/main/java/dto/Berth.java
+++ experimental/HarbourMaster/project/src/main/java/dto/Berth.java
@@ -9,9 +9,6 @@ import javax.persistence.GenerationType;
 
 
 
-
-//ivan
-
 @Entity
 @Table(name="berths")
 public class Berth {
