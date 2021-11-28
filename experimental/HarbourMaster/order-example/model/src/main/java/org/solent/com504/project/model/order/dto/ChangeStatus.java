65e0f75721103d8af0a4f5db4199644e9c0a862b
Jake Ashton <4ashtj48@solent.ac.uk>
2021-11-28 20:51:48 +0000

    pom/ remove class example

--- experimental/HarbourMaster/order-example/model/src/main/java/org/solent/com504/project/model/order/dto/ChangeStatus.java
+++
@@ -1,6 +0,0 @@
-package org.solent.com504.project.model.order.dto;
-
-public enum ChangeStatus {
-
-    REQUESTED, APPROVED, REJECTED
-}
--- experimental/HarbourMaster/order-example/model/src/main/java/org/solent/com504/project/model/resource/dto/ResourceAccess.java
+++
@@ -1,6 +0,0 @@
-package org.solent.com504.project.model.resource.dto;
-
-public enum ResourceAccess {
-
-    EXTERNAL, INTERNAL
-}
