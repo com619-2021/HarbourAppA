77d5f5608b37d0db4e2feb619c513b07bf15a5ed
Jake Ashton <4ashtj48@solent.ac.uk>
2021-12-01 21:17:34 +0000

    Update OrderStatus.java

--- experimental/HarbourMaster/harbour-master/model/src/main/java/org/solent/com504/project/model/dto/OrderStatus.java
+++ experimental/HarbourMaster/harbour-master/model/src/main/java/org/solent/com504/project/model/dto/OrderStatus.java
@@ -6,11 +6,13 @@ package org.solent.com504.project.model.dto;
  * @author jake_
  */
 public enum OrderStatus {
-	CONFIRMED,
-	DENIED,
-	IN_PROGRESS,
-	ACKNOWLEDGED,
-	PLACED,
-	CANCELLED
-}
+    
+    
+    CONFIRMED,
+    DENIED,
+    CHANGE_REQUESTED,
+    IN_PROGRESS,
+    PLACED,
+    CANCELLED
 
+}
